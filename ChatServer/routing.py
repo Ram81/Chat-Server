3b8819bdb3eeca31f2c94dfac4c20f10640a0f45
Ram81 <ramramrakhya81@gmail.com>
2018-06-06 18:59:04 +0530

    Add updates

--- ChatServer/routing.py
+++ ChatServer/routing.py
@@ -7,7 +7,7 @@ application = ProtocolTypeRouter({
 	# (http->django views is added by default)
 	'websocket': AuthMiddlewareStack(
         URLRouter(
-            Chat.routing.websocket_urlpatterns
+            chat.routing.websocket_urlpatterns
         )
     ),
 })
